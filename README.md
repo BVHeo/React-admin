46ed85a6cd5af3152e61216def078266276a69ed
BVHeo <1427486959@qq.com>
2019-11-29 11:24:47 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # React-admin
 后台管理
+
+
+
+yarn (安装依赖)
+
+npm start(运行项目) 
+
+访问端口5000
+
---
+++ public/precache-manifest.8607295ec5fbde1436414072f1f9588f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feace3f9f31866293f6bd2149a81f59c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca904b9fc3224556592",
+    "url": "/static/css/2.e1a1cdb5.chunk.css"
+  },
+  {
+    "revision": "7e1cd7604a348e371ac6",
+    "url": "/static/css/main.8ebb6e84.chunk.css"
+  },
+  {
+    "revision": "fca904b9fc3224556592",
+    "url": "/static/js/2.b84c372a.chunk.js"
+  },
+  {
+    "revision": "7e1cd7604a348e371ac6",
+    "url": "/static/js/main.0d500773.chunk.js"
+  },
+  {
+    "revision": "74545b4eb0ace3cd6a27",
+    "url": "/static/js/runtime~main.4dad9c15.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  }
+]);
